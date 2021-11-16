63600df99b8e5cf7dcfcc01526082e6780f5c931
Maxime VALLE <maxime.valllle@gmail.com>
2021-11-16 14:03:38 +0100

    M (100mm) et M (150 mm) en mode subplot

--- dibuja_figuras.m
+++ dibuja_figuras.m
@@ -34,6 +34,7 @@ DELTA = sqrt(2./(2*pi*FREQS*4*pi*1e-7*sigma));
 
 %%
 figure(1)
+subplot(1,2,1);
 plot(1e3*SIM_RX_100.d_x, 1e6*SIM_RX_100.M_TX_RX,'b-');
 hold on;
 grid on;
@@ -43,6 +44,17 @@ legend({'theorical M with z = 100 mm','experimental M with z = 100 mm'});
 xlabel({"x shift","(mm)"});
 ylabel({"Mutual inductance M","(uH)"});
 title({"variation of mutual inductance M with the shift in x","(coils separated with z = 10 cm)"});
+
+subplot(1,2,2);
+plot(1e3*SIM_RX_150.d_x, 1e6*SIM_RX_150.M_TX_RX,'b-');
+hold on;
+grid on;
+%plot(SIM_RX_150.d_x, SIM_RX_150.M_RX_TX,'k-')
+plot(1e3*MED.M_150.d(IND_F,:), 1e6*MED.M_150.M(IND_F,:),'bo');
+legend({'theorical M with z = 150 mm','experimental M with z = 150 mm'});
+xlabel({"x shift","(mm)"});
+ylabel({"Mutual inductance M","(uH)"});
+title({"variation of mutual inductance M with the shift in x","(coils separated with z = 15 cm)"});
  
 %%
 % la resistance de conduction DC ne varie pas avec le shift (diff valeurs
