df694783f167a74e8bd0ec3d725763861c48fcce
Maxime VALLE <maxime.valllle@gmail.com>
2021-12-03 22:07:43 +0100

    modification nom des répertoires

--- dibuja_figuras.m
+++ dibuja_figuras.m
@@ -12,12 +12,12 @@ DIR_SIM = 'SIM\';
 
 load([DIR_MED 'MED.mat']);
 
-load([DIR_SIM 'RX_100\SIM.mat']);
-SIM_RX_100 = SIM;
+load([DIR_SIM 'Z_100\SIM.mat']);
+SIM_Z_100 = SIM;
 clear('SIM');
 
-load([DIR_SIM 'RX_150\SIM.mat']);
-SIM_RX_150 = SIM;
+load([DIR_SIM 'Z_150\SIM.mat']);
+SIM_Z_150 = SIM;
 clear('SIM');
 
 IND_F = 5;
@@ -34,14 +34,15 @@ DELTA = sqrt(2./(2*pi*FREQS*4*pi*1e-7*sigma));
 
 %%
 figure(1)
-plot(1e3*SIM_RX_100.d_x, 1e6*SIM_RX_100.M_TX_RX,'b-');
+plot(1e3*SIM_Z_100.d_x, 1e6*SIM_Z_100.M_TX_RX,'b-');
 hold on;
 grid on;
-%plot(SIM_RX_150.d_x, SIM_RX_150.M_RX_TX,'k-')
-plot(1e3*MED.L_100.d(IND_F,:), 1e6*MED.M_100.M(IND_F,:),'bo');
+%plot(SIM_Z_150.d_x, SIM_Z_150.M_RX_TX,'k-')
+plot(1e3*MED.M_100.d(IND_F,:), 1e6*MED.M_100.M(IND_F,:),'bo');
 
-plot(1e3*SIM_RX_150.d_x, 1e6*SIM_RX_150.M_TX_RX,'g-');
-%plot(SIM_RX_150.d_x, SIM_RX_150.M_RX_TX,'k-')
+plot(1e3*SIM_Z_150.d_x, 1e6*SIM_Z_150.M_TX_RX,'g-');
+
+%plot(SIM_Z_150.d_x, SIM_Z_150.M_RX_TX,'k-')
 plot(1e3*MED.M_150.d(IND_F,:), 1e6*MED.M_150.M(IND_F,:),'go');
 legend({'theorical M with z = 100 mm', 'experimental M with z = 100 mm', 'theorical M with z = 150 mm','experimental M with z = 150 mm'},'location','south');
 xlabel({"x shift","(mm)"});
@@ -51,8 +52,8 @@ title({"variation of mutual inductance M with the shift in x"});
 %%
 % la resistance de conduction DC ne varie pas avec le shift (diff valeurs
 % de LONG_RX. On instancie une fois pour chaque fréquence balayé.
-R_cond_RX = ones(size(FREQS))*SIM_RX_150.LONG_RX(1,IND_DIST)/(ns*sigma*pi*rs^2);
-R_prox_RX = ns*(2*pi/sigma)*(rs./DELTA).^4.*(SIM_RX_150.H02_RX(1,IND_DIST)./SIM_RX_150.I_RX(1,IND_DIST)^2);
+R_cond_RX = ones(size(FREQS))*SIM_Z_150.LONG_RX(1,IND_DIST)/(ns*sigma*pi*rs^2);
+R_prox_RX = ns*(2*pi/sigma)*(rs./DELTA).^4.*(SIM_Z_150.H02_RX(1,IND_DIST)./SIM_Z_150.I_RX(1,IND_DIST)^2);
 
 figure()
 %plot(FREQS/1e3, 1e3*R_cond_RX.*R_prox_RX,'k-');
